199c1cf4b21c47b897d79ccf353cb96ece85777a
Ahmad Ali <amdali97@gmail.com>
2023-12-26 23:16:00 +0000

    sdxl code

--- src/predict_sdxlturbo.py
+++ src/predict_sdxlturbo.py
@@ -2,26 +2,16 @@
 import os
 from typing import List
 
+from diffusers import AutoPipelineForText2Image
 import torch
-from diffusers import (
-    StableDiffusionPipeline,
-    PNDMScheduler,
-    LMSDiscreteScheduler,
-    DDIMScheduler,
-    EulerDiscreteScheduler,
-    EulerAncestralDiscreteScheduler,
-    DPMSolverMultistepScheduler,
-)
-# from diffusers.pipelines.stable_diffusion.safety_checker import (
-#     StableDiffusionSafetyChecker,
-# )
 
 
 # MODEL_ID = "Lykon/dreamshaper-7"
-MODEL_ID = "stablediffusionapi/reliberatev2"
+MODEL_ID = "stabilityai/sdxl-turbo"
 MODEL_CACHE = "/workspace/.cache/"
-SAFETY_MODEL_ID = "CompVis/stable-diffusion-safety-checker"
 NEGATIVE_PROMPT = "worst quality, normal quality, low quality, low res, blurry, text, watermark, logo, banner, extra digits, cropped, jpeg artifacts, signature, username, error, sketch ,duplicate, ugly, monochrome, horror, geometry, mutation, disgusting"
+
+
 
 
 CACHE_DIR = "/workspace/.cache/"
@@ -36,12 +26,10 @@ class Predictor:
         #     local_files_only=True,
         # )
 
-        self.pipe = StableDiffusionPipeline.from_pretrained(
-            MODEL_ID,
-            safety_checker=None,
-            # safety_checker=safety_checker,
-            cache_dir=MODEL_CACHE,
-        ).to("cuda")
+        self.pipe = AutoPipelineForText2Image.from_pretrained(MODEL_ID, 
+                                                              torch_dtype=torch.float16,
+                                                              variant="fp16")
+        self.pipe.to("cuda")
 
         self.pipe.enable_xformers_memory_efficient_attention()
 
@@ -56,8 +44,8 @@ class Predictor:
         output = self.pipe(
             prompt=prompt,
             negative_prompt= NEGATIVE_PROMPT,
-            num_inference_steps=num_inference_steps,
-            generator=generator,
+            num_inference_steps=4, 
+            guidance_scale=0.0
         )
 
         return output.images[0]
@@ -72,6 +60,8 @@ if __name__ == "__main__":
 if __name__ == "__main__":
 
     prompt = "a cat jumping over a dog"
+    prompt = "A cinematic shot of a baby racoon wearing an intricate italian priest robe."
+
     output = predictor.predict(prompt)
     output.save("output.png")
 
